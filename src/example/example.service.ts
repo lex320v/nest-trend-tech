1b2176c697bfc18e1c47d5433647f2f48f0d70cb
alexandr <sasha320v@yandex.ru>
2024-06-27 23:38:02 +0300

    throw exception

--- src/example/example.service.ts
+++ src/example/example.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Example } from './schemas/example.schema';
 import { Model } from 'mongoose';
@@ -7,9 +7,14 @@ import { Model } from 'mongoose';
 export class ExampleService {
   constructor(@InjectModel(Example.name) private readonly exampleModel: Model<Example>) {}
 
-  example(search: string) {
-    return this.exampleModel.findOne({
+  async example(search: string) {
+    const result = await this.exampleModel.findOne({
       name: search,
     }).exec();
+    if (!result) {
+      throw new HttpException('incorrect search', HttpStatus.BAD_REQUEST);
+    }
+
+    return result;
   }
 }
