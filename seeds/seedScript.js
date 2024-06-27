edd8a502b4553187080e2aeed62b4af84eeb3740
alexandr <sasha320v@yandex.ru>
2024-06-27 23:18:25 +0300

    loadtest

--- seeds/seedScript.js
+++ seeds/seedScript.js
@@ -1,4 +1,5 @@
 const { randomUUID } = require('node:crypto');
+const fs = require('node:fs');
 const MongoClient = require('mongodb').MongoClient;
 
 async function seedDB() {
@@ -11,16 +12,19 @@ async function seedDB() {
     const collection = client.db("nest").collection("examples");
     await collection.drop();
 
+    const uuids = [];
     let bathArray = [];
     for (let i = 1; i <= 10_000_000; i++) {
       bathArray.push({ name: randomUUID() });
 
       if (i % 100_000 === 0) {
         await collection.insertMany(bathArray);
+        uuids.push(bathArray[0].name);
         bathArray = [];
       }
     }
 
+    await fs.promises.writeFile('uuids.json', JSON.stringify(uuids));
     await client.close()
   } catch (err) {
     console.log(err.stack);
--- test/loadtest.spec.ts
+++ test/loadtest.spec.ts
@@ -4,6 +4,14 @@ import { URL } from 'https://jslib.k6.io/url/1.0.0/index.js';
 import http from 'k6/http';
 // @ts-ignore
 import { check, sleep } from 'k6';
+// @ts-ignore
+import { SharedArray } from 'k6/data';
+
+let count = 0;
+const uuids = new SharedArray('some data name', function () {
+  // @ts-ignore
+  return JSON.parse(open('../seeds/uuids.json'));
+});
 
 export const options = {
   vus: 10,
@@ -11,11 +19,13 @@ export const options = {
 }
 
 export default function () {
-  const name = 'qweewq';
-
   const url = new URL('http://localhost:3000/example');
+
+  const name = uuids[Math.floor(Math.random() * uuids.length)];
   url.searchParams.append('search', name);
+
   const res = http.get(url.toString());
   check(res, { 'status was 200': (r) => r.status == 200 });
+  check(res, { 'correct body': (r) => JSON.parse(r.body).name === name });
   sleep(1);
 }
