4159061bc502ff07afa19406d46dfcc332fda46f
alexandr <sasha320v@yandex.ru>
2024-06-28 14:43:04 +0300

    fix

--- src/example/schemas/example.schema.ts
+++ src/example/schemas/example.schema.ts
@@ -1,7 +1,4 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
-
-export type ExampleDocument = HydratedDocument<Example>;
 
 @Schema()
 export class Example {
